90afd6a6820da807a192e4bb2daf52296db72ed5
vantamin <vantablack.min@gmail.com>
2021-01-29 14:49:23 +0900

    Refactor Prettier

--- src/google_images_download.py
+++ src/google_images_download.py
@@ -1,8 +1,13 @@
-from google_images_download import google_images_download  # importing the library
+from google_images_download import google_images_download
 
-response = google_images_download.googleimagesdownload()  # class instantiation
+response = google_images_download.googleimagesdownload()
 
-arguments = {"keywords": "Polar bears,baloons,Beaches",
-             "limit": 20, "print_urls": True}  # creating list of arguments
-paths = response.download(arguments)  # passing the arguments to the function
-print(paths)  # printing absolute paths of the downloaded images
+arguments = {
+  "keywords": "Polar bears,baloons,Beaches",
+  "limit": 20,
+  "print_urls": True
+}
+
+paths = response.download(arguments)
+
+print(paths)
