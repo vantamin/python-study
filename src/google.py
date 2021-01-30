9b23af7efec8069f4bf5ea6f8105a69b2dea5234
vantamin <vantablack.min@gmail.com>
2021-01-30 23:19:53 +0900

    Update google.py

--- src/google.py
+++ src/google.py
@@ -6,51 +6,56 @@ import time
 
 import urllib.request
 
-driver = webdriver.Chrome("/Users/we/python-study/chromedriver")
+def google_images_download(keyword):
+  driver = webdriver.Chrome("/Users/we/python-study/chromedriver")
 
-driver.get("https://www.google.co.kr/imghp?hl=ko&tab=ri&ogbl")
+  driver.get("https://www.google.co.kr/imghp?hl=ko&tab=ri&ogbl")
 
-elem = driver.find_element_by_name("q")
+  elem = driver.find_element_by_name("q")
 
-elem.send_keys("조코딩")
+  elem.send_keys(keyword)
 
-elem.send_keys(Keys.RETURN)
+  elem.send_keys(Keys.RETURN)
 
-SCROLL_PAUSE_TIME = 1.5
+  SCROLL_PAUSE_TIME = 1.5
 
-last_height = driver.execute_script("return document.body.scrollHeight")
+  last_height = driver.execute_script("return document.body.scrollHeight")
 
-while True:
-  driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+  while True:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
 
-  time.sleep(SCROLL_PAUSE_TIME)
+    time.sleep(SCROLL_PAUSE_TIME)
 
-  new_height = driver.execute_script("return document.body.scrollHeight")
+    new_height = driver.execute_script("return document.body.scrollHeight")
 
-  if new_height == last_height:
+    if new_height == last_height:
+      try:
+        driver.find_element_by_css_selector(".mye4qd").click()
+      except:
+        break
+
+    last_height = new_height
+
+  images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+
+  count = 1
+
+  for image in images:
     try:
-      driver.find_element_by_css_selector(".mye4qd").click()
+      image.click()
+
+      time.sleep(2)
+
+      imgUrl = driver.find_element_by_xpath("/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img").get_attribute("src")
+
+      urllib.request.urlretrieve(imgUrl, "download/" + keyword + "/"
+      +
+      str(count)
+      +
+      ".jpg")
+
+      count = count + 1
     except:
-      break
+      pass
 
-  last_height = new_height
-
-images, elem = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
-
-count = 1
-
-for image in images:
-  try:
-    image.click()
-
-    time.sleep(2)
-
-    imgUrl = driver.find_element_by_xpath("/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div[1]/div[1]/div/div[2]/a/img").get_attribute("src")
-
-    urllib.request.urlretrieve(imgUrl, str(count) + ".jpg")
-
-    count = count + 1
-  except:
-    pass
-
-driver.close()
+  driver.close()
