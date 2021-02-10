f05621a1822f52230217c74cb1347b2c6ed71e0f
vantamin <vantablack.min@gmail.com>
2021-02-10 12:29:59 +0900

    Update Face Crop to URL

--- src/face_crop.py
+++ src/face_crop.py
@@ -1,3 +1,7 @@
+from urllib.request import urlopen
+
+import numpy as np
+
 import cv2
 
 face_cascade = cv2.CascadeClassifier("lib/python3.8/site-packages/cv2/data/haarcascade_frontalface_default.xml")
@@ -5,16 +9,18 @@ face_cascade = cv2.CascadeClassifier("lib/python3.8/site-packages/cv2/data/haarc
 eyes_cascade = cv2.CascadeClassifier("lib/python3.8/site-packages/cv2/data/haarcascade_eye.xml")
 
 def face_crop(url, filename):
-    retval = cv2.imread(url)
+    retval = urlopen(url)
 
-    dst = cv2.cvtColor(retval, cv2.COLOR_BGR2GRAY)
+    dst = np.asarray(bytearray(retval.read()), dtype="uint8")
+
+    dst = cv2.imdecode(dst, cv2.COLOR_BGR2GRAY)
 
     faces = face_cascade.detectMultiScale(dst, 1.3, 5)
 
     i = 1
 
     for x, y, w, h in faces:
-        img = retval[y - int(h / 4):y + h + int(h / 4), x - int(w / 4):x + w + int(w / 4)]
+        img = dst[y - int(h / 4):y + h + int(h / 4), x - int(w / 4):x + w + int(w / 4)]
 
         cv2.imwrite(filename + "_" + str(i) + ".jpg", img)
 
